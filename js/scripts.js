d19b94f65f30400e83cb8fad94fcca2489ec2801
geomio <geoff.millner22@gmail.com>
2021-03-02 10:53:00 -0800

    fixed js function to work and pass user input to text area

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,22 @@ $(document).ready(function() {
     const streetInput = $("input#street").val();
     const cityInput= $("input#city").val();
     const stateInput = $("input#state").val();
+    const zipInput = $("input#zip").val();
     const countryInput = $("input#country").val();
     const phoneInput = $("input#phone").val();
     const emailInput = $("input#email").val();
-    const birthdayInput = $("input#birthday").val();
+    
     const appoInput = $("input#appo").val();
 
     $(".name").text(nameInput);
     $(".street").text(streetInput);
     $(".city").text(cityInput);
     $(".state").text(stateInput);
+    $(".zip").text(zipInput);
     $(".country").text(countryInput);
     $(".phone").text(phoneInput);
-    $(".email").text(emailinput);
-    $(".birthday").text(birthdayinput);
-    $(".appo").text(appoinput);
+    $(".email").text(emailInput);
+    $(".appo").text(appoInput);
   
     event.preventDefault();
   });
